cb181f64ee70159084fefb3536d0ce29cf8d842d
zoshigayan <hello@zoshigayan.net>
2022-05-13 03:56:30 +0900

    :+1: add SQL logger

--- db/db.go
+++ db/db.go
@@ -3,14 +3,27 @@ package db
 import (
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"log"
+	"os"
+	"time"
 )
 
 var db *gorm.DB
 var err error
 
 func Init() {
-	db, err = gorm.Open(sqlite.Open("zocms.db"), &gorm.Config{})
+	db, err = gorm.Open(sqlite.Open("zocms.db"), &gorm.Config{
+		Logger: logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+			logger.Config{
+				SlowThreshold:             time.Second, // Slow SQL threshold
+				LogLevel:                  logger.Info, // Log level
+				IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+				Colorful:                  false,       // Disable color
+			},
+		),
+	})
 	if err != nil {
 		panic("DB connection failed.")
 	}
